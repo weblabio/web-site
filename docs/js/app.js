f771143f81d415725a617c7eaf10826a387d6884
IgorArt <weblab.imporg@gmail.com>
2022-01-14 16:14:59 +0300

    turnoff mouse move

--- docs/js/app.js
+++ docs/js/app.js
@@ -75,7 +75,7 @@
     // Event handling
     function addListeners() {
         if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
+            // window.addEventListener('mousemove', mouseMove);
         }
         window.addEventListener('scroll', scrollCheck);
         window.addEventListener('resize', resize);
